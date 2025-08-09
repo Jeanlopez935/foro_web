ae87145f9e20720ad526442fdd1f67c76b053484
vanessahernan <hernamdezvanessa16@gmail.com>
2025-08-09 17:28:36 -0400

    si

--- users/models.py
+++ users/models.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-# class User(AbstractUser):
-#     groups = models.ManyToManyField(
-#         'auth.Group',
-#         related_name='custom_user_set',
-#         blank=True,
-#         help_text='The groups this user belongs to...',
-#         verbose_name='groups'
-#     )
-#     user_permissions = models.ManyToManyField(
-#         'auth.Permission',
-#         related_name='custom_user_set',
-#         blank=True,
-#         help_text='Specific permissions for this user...',
-#         verbose_name='user permissions'
-#     )
+class User(AbstractUser):
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='The groups this user belongs to...',
+        verbose_name='groups'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',
+        blank=True,
+        help_text='Specific permissions for this user...',
+        verbose_name='user permissions'
+    )
